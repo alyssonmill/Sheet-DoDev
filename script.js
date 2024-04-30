be3aff27c1d6893d3a156bc040e2f31bd8e7bdbc
Alysson Miller <alyssonmillerdev@gmail.com>
2024-04-30 17:17:12 +0100

    Strugling on Authentications

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const credentials = require('./credentials.json');
+const arquivo = require('./arquivo.json');
+const { JWT } = require('google-auth-library');
+const file = require('node-fetch');
+
+const SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file'
+];
+
+const jwt = new JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    scopes: SCOPES,
+});
+
+
+
+
+
+
+
